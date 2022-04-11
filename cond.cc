4f527c6cdae50372aba0b8e7e741339afcf3a9d8
Michael Madden <mikemadden42@icloud.com>
2022-04-10 21:28:34 -0500

    Initial cond.cc

---
+++ cond.cc
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main() {
+    int x = 0;
+    if (x > 0)
+        printf("positive...\n");
+    else if (x < 0)
+        printf("negative..\n");
+    else
+        printf("zero...\n");
+}
